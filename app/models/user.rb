22cc003caa0ae874571c90afc6d6c0f65b9d1718
EC2 Default User <ec2-user@ip-172-31-81-36.ec2.internal>
2021-04-24 08:45:18 +0000

    エラー修正sign upページの編集完

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+        # （パスワードの正確性を検証）,（ユーザ登録や編集、削除）
+        # （パスワードをリセット）,（ログイン情報を保存）,（emailのフォーマットなどのバリデーション）
+  has_many :books, dependent: :destroy
+  # has_manyの後には、1:Nの関係になるモデル名を複数形で記述します。
+
+end
